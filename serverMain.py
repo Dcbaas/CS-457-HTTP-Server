2353b8883a23a0daa29a45043ce614452dfe96d0
Dcbaas <Dcbaas@users.noreply.github.com>
2019-04-12 15:39:18 -0400

    fixed flag checking in serverMain

--- serverMain.py
+++ serverMain.py
@@ -11,12 +11,12 @@ defaultLog = ''
 args = getopt.getopt(sys.argv[1:], 'p:d:l:')
 for arg in args:
     if len(arg) != 0:
-        if arg[0] == '-p':
-            defaultPort = int(arg[1])
-        elif arg[0] == '-d':
-            defaultDoc = arg[1]
-        elif arg[0] == '-l':
-            defaultLog = arg[1]
+        if '-p' in arg[0]:
+            defaultPort = int(arg[0][1])
+        elif '-d' in arg[0]:
+            defaultDoc = arg[0][1]
+        elif '-l' in arg[0]:
+            defaultLog = arg[0][1]
         else:
             print('Error: invaild flag')
             exit(1)
